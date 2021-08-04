e1a332f5bebcf3f14585c09694ef71549da5ae86
skoobydont <31373232+skoobydont@users.noreply.github.com>
2021-08-03 20:33:36 -0400

    some more reach stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import * as reach from '@reach-sh/stdlib/ALGO';
+import { useRef } from 'react';
 
 function App() {
+  const account = useRef();
+  const balance = useRef();
+
+  // helpers
+  const getAccount = async () => {
+    account.current = await reach.getDefaultAccount();
+    console.log('account current: ', account.current);
+  }
+  const getBalance = async () => {
+    /**
+     * balanceOf returns in microalgos
+     */
+    let rawBalance = await reach.balanceOf(account.current);
+    /**
+     * format microalgos to Algos
+     */
+    balance.current = reach.formatCurrency(rawBalance, 4);
+    console.log('balance current: ', balance.current);
+  }
+  /**
+   * Connect to Wallet
+   * @async
+   * @fires getAccount
+   * @fires getBalance
+   */
+  const connectWallet = async () => {
+    await getAccount();
+    await getBalance();
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={connectWallet}>connect wallet</button>
     </div>
   );
 }
