da7b10073cdd98b6e10b7d1fc51f6bde66430d4b
skoob <ajskubak@gmail.com>
2021-09-25 17:59:03 -0400

    handle decrypting objs & attrs

--- src/utilities/encryptAttr.js
+++ src/utilities/encryptAttr.js
@@ -1,14 +1,28 @@
 import Cryptr from 'cryptr';
 import getAttrKey from './getAttrKey';
-
-export const encryptObj = (obj) => {
+/**
+ * Encrypt an Object & associated Attribute
+ * @param {Object} obj to encrypt
+ * @param {Boolean} encrypt or decrypt
+ * @returns {Object}
+ */
+export const encryptObj = (obj, encrypt = true) => {
   let result = {};
   if (typeof obj === 'object') {
     const attrCryptr = new Cryptr(getAttrKey('attribute'));
     Object.keys(obj).forEach((atr) => {
-      const objCryptr = new Cryptr(getAttrKey(atr));
-      const eAttr = attrCryptr.encrypt(atr);
-      const eVal = objCryptr.encrypt(obj[atr]);
+      let objCryptr = {};
+      if (!encrypt) {
+        objCryptr = new Cryptr(getAttrKey(attrCryptr.decrypt(atr)));
+      } else {
+        objCryptr = new Cryptr(getAttrKey(atr));
+      }
+      const eAttr = encrypt
+        ? attrCryptr.encrypt(atr)
+        : attrCryptr.decrypt(atr);
+      const eVal = encrypt
+        ? objCryptr.encrypt(obj[atr])
+        : objCryptr.decrypt(obj[atr]);
       result = {
         ...result,
         [eAttr]: eVal,
@@ -18,11 +32,13 @@ export const encryptObj = (obj) => {
   return result;
 };
 
-export default (val, eKey) => {
+export default (val, eKey, encrypt = true) => {
   if (!eKey || eKey?.length < 1) return val;
   const cryptr = new Cryptr(eKey);
   try {
-    return cryptr.encrypt(val);
+    return encrypt
+      ? cryptr.encrypt(val)
+      : cryptr.decrypt(val);
   } catch (e) {
     throw new Error(e);
   }
