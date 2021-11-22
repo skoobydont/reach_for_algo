8bd713bee46255527f63cfe775bbabc04d1fdf60
skoobydont <31373232+skoobydont@users.noreply.github.com>
2021-11-22 12:02:32 -0500

    init profile page stuff

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,4 +1,22 @@
+/* global AlgoSigner */
+import React, { useState, useCallback, useRef } from 'react';
+// MUI
+import {
+  TextField,
+  Paper,
+  Button,
+  Typography,
+} from '@material-ui/core';
+
+
 const ProfilePage = () => {
+  // input
+  // 1. ssn
+  // 2. contact info
+  // 2a. f & l name
+  // 2b. address
+  // 2c. phone
+  // 2d. email
   return (
     <div>Profile Page</div>
   );
