a8e160a78475817411eca33f55b4221ea668cf55
skoobydont <31373232+skoobydont@users.noreply.github.com>
2021-12-27 18:39:07 -0500

    get all assets for env wallet in profile page & display name

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,4 +1,3 @@
-/* global AlgoSigner */
 import React, { useState, useCallback, useRef } from 'react';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
@@ -19,8 +18,31 @@ const ProfilePage = (props) => {
     handleAlgoParams,
     algoGlobal,
     handleAppGlobalState,
+    handleGetAccountInfo,
+    handleGetAssetInfo,
   } = props;
+
+  const [assets, setAssets] = useState(null);
   
+  const getWalletAssetsInfo = async (walletAddress) => {
+    const walletInfo = await handleGetAccountInfo(walletAddress);
+    if (typeof walletInfo === 'object' && Object.keys(walletInfo).includes('account')) {
+      const assetInfoRes = [];
+      if (Array.isArray(walletInfo.account?.assets)) {
+        walletInfo.account.assets.forEach((asset) => {
+          assetInfoRes.push(handleGetAssetInfo(asset['asset-id']));
+        });
+      }
+      console.log('asset info res', assetInfoRes);
+      const assetInfoResResolved = await Promise.all(assetInfoRes);
+      console.log('asset info res after promise all', assetInfoResResolved);
+      if (Array.isArray(assetInfoResResolved) && assetInfoResResolved?.length > 0) {
+        setAssets(assetInfoResResolved);
+      }
+      return assetInfoRes;
+    }
+  }
+
   return (
     <div>
       <>
@@ -31,52 +53,21 @@ const ProfilePage = (props) => {
           Check AlgoSigner
         </Button> */}
         <Button
-          onClick={handleAlgoAccount}
+          onClick={() => getWalletAssetsInfo(process.env.REACT_APP_BASE_WALLET_ADDRESS)}
           variant="contained"
         >
-          Get Algo Accounts
+          Get Assets
         </Button>
-        <Button
-          onClick={handleAlgoParams}
-          variant="contained"
-        >
-          Algo Params
-        </Button>
-        <Button
-          onClick={handleAppGlobalState}
-          variant="contained"
-        >
-          App Global State
-        </Button>
-        {algoSignerInstalled.current !== undefined
-          ? 'AlgoSigner Detected'
-          : 'AlgoSigner Not Detected'}
-        {algoAccount.current !== null
-          && algoAccount.current?.length > 0
-          && algoAccount.current?.map((act, i) => {
-            return <div>Account {`${i} : ${act.address}`}</div>
-          })}
-        {/* {!algoSignerInstalled
-          && <CheckAlgoSigner
-                title=""
-                ButtonText="Detect AlgoSigner"
-                result={algoSignerInstalled}
-                setResult={handleAlgoSignerInstalled}
-              />}
-        {algoSignerInstalled
-          && account.current?.length === 0
-          && (
-            <GetAccounts
-              ButtonText="Connect Account"
-              setResult={handleAlgoAccount}
-            />)}
-        {account.current?.length !== 0
-          && (
-            <GetParams
-              ButtonText="Get Params"
-              setResult={handleAlgoParams}
-            />
-          )} */}
+        {Array.isArray(assets) && assets?.length > 0
+          ? (
+            <div>
+              {assets?.map(({ asset }, i) => (
+                <div key={i}>
+                  <Typography>{asset?.params?.name}</Typography>
+                </div>
+              ))}
+            </div>
+          ) : null}
       </>
     </div>
   );
