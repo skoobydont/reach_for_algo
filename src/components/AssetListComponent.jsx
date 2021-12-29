d92d490f3dffaeee0efd735f9bfe77099175e542
skoobydont <31373232+skoobydont@users.noreply.github.com>
2021-12-29 14:56:32 -0500

    added asset name to card, collapse handler. still need opt-in workflow

--- src/components/AssetListComponent.jsx
+++ src/components/AssetListComponent.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 // MUI
 import Card from '@material-ui/core/Card';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/styles';
-import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Collapse from '@material-ui/core/Collapse';
 import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
 // Icons
 import LockIcon from '@material-ui/icons/Lock';
 import LockOpenIcon from '@material-ui/icons/LockOpen';
@@ -25,6 +25,12 @@ const useStyles = makeStyles((theme) => ({
     maxWidth: theme.spacing(50),
     minWidth: theme.spacing(30),
   },
+  cardActions: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: `0px ${theme.spacing(1)}px`,
+  },
 }));
 
 const AssetListComponent = (props) => {
@@ -32,6 +38,40 @@ const AssetListComponent = (props) => {
     assets,
   } = props;
   const classes = useStyles();
+  /**
+   * Generate Initial Asset Collapse Object
+   * @returns {Object} keys as asset index and value false (so all are default collapsed)
+   */
+  const initAssetCollapse = () => {
+    let result = {};
+    if (Array.isArray(assets) && assets?.length > 0) {
+      assets?.forEach(({ asset }) => {
+        result = {
+          ...result,
+          [`${asset?.index}`]: false,
+        }
+      });
+    }
+    return result;
+  }
+  const [assetCollapse, setAssetCollapse] = useState(initAssetCollapse());
+  /**
+   * Handle Toggle Asset Collapse At Index Given
+   * @param {number} index the numerical index of the ASA
+   * @returns {null}
+   * @fires setAssetCollapse update attribute at passed index
+   */
+  const handleToggleAssetCollapse = (index) => {
+    if (Object.keys(assetCollapse).includes(String(index))) {
+      setAssetCollapse({
+        ...assetCollapse,
+        [index]: !assetCollapse[index],
+      });
+    }
+    return null;
+  }
+  // TDOD: const handleOptInAsset()
+  // https://dappradar.com/blog/algorand-dapp-development-2-standard-asset-management
 
   return (
     <div className={classes.root}>
@@ -43,7 +83,33 @@ const AssetListComponent = (props) => {
             key={i}
             className={classes.card}
           >
-            
+            <CardContent>
+              <Typography>
+                {asset?.params?.name}
+              </Typography>
+            </CardContent>
+            <CardActions className={classes.cardActions}>
+              <IconButton
+                onClick={() => handleToggleAssetCollapse(asset?.index)}
+              >
+                {assetCollapse[asset?.index]
+                  ? <ExpandLessIcon />
+                  : <ExpandMoreIcon />}
+              </IconButton>
+              <Button>
+                Opt-In
+              </Button>
+            </CardActions>
+            <Collapse
+              in={assetCollapse[asset?.index]}
+              timeout="auto"
+              unmountOnExit
+              className={classes.card}
+            >
+              <CardContent>
+                <Typography>Show Collapsed Info For Asset At Index: {asset?.index}</Typography>
+              </CardContent>
+            </Collapse>
           </Card>
         );
       })}
