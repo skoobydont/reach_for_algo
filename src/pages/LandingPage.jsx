aaa227b126dc1562e9dca9a55e8429858ccb1f18
skoobydont <31373232+skoobydont@users.noreply.github.com>
2022-01-10 13:38:00 -0500

    install myalgoconnect sdk & landing page updates

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,6 +2,11 @@ import React, { useRef, useEffect, useState } from 'react';
 // MUI
 import { makeStyles } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
+// import Select from '@material-ui/core/Select';
+// import MenuItem from '@material-ui/core/MenuItem';
+// import TextField from '@material-ui/core/TextField';
+// import Button from '@material-ui/core/Button';
+import LinearProgress from '@material-ui/core/LinearProgress';
 // Custom
 import AssetListComponent from '../components/AssetListComponent';
 
@@ -28,19 +33,53 @@ const LandingPage = (props) => {
     algosdk,
     algodClient,
     handleSetAccount,
+    ledger,
+    setLedger,
   } = props;
 
   const assets = useRef();
   const [refresh, setRefresh] = useState(false);
-
+  const [walletInput, setWalletInput] = useState('');
+  /**
+   * Handle Updating Account Info
+   * @param {Object} addr wallet address information object returned from algo adk indexer
+   * @fires handleSetAccount
+   * @returns {Object} the updated account info
+   */
   const handleUpdatingAccountInfo = async (addr) => {
     const updatedAccountInfo = await handleGetAccountInfo(addr);
-    console.log('the updated account info: ', updatedAccountInfo);
     handleSetAccount(updatedAccountInfo);
     return updatedAccountInfo;
   };
-
-  
+  /**
+   * Handle Select Ledger Option Change
+   * @param {Object} e event object
+   */  
+  // const handleSelectLedgerChange = (e) => {
+  //   setLedger(e.target.value);
+  // };
+  /**
+   * Handle Wallet Text Input Change
+   * @param {Object} e event object
+   */
+  // const handleWalletInputChange = (e) => {
+  //   setWalletInput(e.target.value);
+  // };
+  /**
+   * Handle Connect Wallet Submit
+   * @async
+   * @param {Object} e event object
+   */
+  // const handleConnectWalletSubmit = async (e) => {
+  //   e?.preventDefault();
+  //   // get wallet info
+  //   // update wallet info
+  //   if (walletInput !== null) {
+  //     setRefresh(true);
+  //     await handleUpdatingAccountInfo(walletInput);
+  //   }
+  //   setRefresh(false);
+  // };  
   // get assets from main wallet address
   useEffect(() => {
     /**
@@ -52,6 +91,7 @@ const LandingPage = (props) => {
      *  only want to show assets in wallet that are also in main wallet i guess)
      */
     const getWalletAssetsInfo = async (walletAddress) => {
+      setRefresh(true);
       const walletInfo = await handleGetAccountInfo(walletAddress);
       if (typeof walletInfo === 'object' && Object.keys(walletInfo).includes('account')) {
         const assetInfoRes = [];
@@ -67,7 +107,7 @@ const LandingPage = (props) => {
           assets.current = assetInfoResResolved;
         }
       }
-      setRefresh(true);
+      setRefresh(false);
       return walletInfo;
     } 
     if (assets.current === undefined) {
@@ -75,12 +115,43 @@ const LandingPage = (props) => {
       getWalletAssetsInfo(process.env.REACT_APP_BASE_WALLET_ADDRESS);
     }
   }, [assets, handleGetAccountInfo, handleGetAssetInfo]);
-  console.log('the assets: ', assets);
-  return (
+  // console.log('the assets: ', assets);
+  console.log('the ledger', ledger);
+  return refresh ? <LinearProgress /> : (
     <div className={classes.root}>
       <Typography variant="subtitle1" color="primary">
         Real Estate For The Digital Age
       </Typography>
+      {user.current === null || user.current === undefined
+        ? (
+          <>
+            {/* TODO: When / if redesign, if wallet & net based,
+              implement workflow to capture relevant info */}
+            {/* <Select
+              label="Ledger"
+              title="Ledger"
+              onChange={handleSelectLedgerChange}
+              value={ledger}
+            >
+              {['', 'MainNet', 'TestNet']?.map((lgr, i) => (
+                <MenuItem
+                  value={lgr}
+                  key={i}
+                >
+                  {lgr}
+                </MenuItem>
+              ))}
+            </Select>
+            <form onSubmit={handleConnectWalletSubmit}>
+              <TextField
+                label="Wallet Address"
+                value={walletInput}
+                onChange={handleWalletInputChange}
+              />
+              <Button type="submit">Connect Wallet</Button>
+            </form> */}
+          </>
+        ) : null}
       {Array.isArray(assets.current) && assets.current?.length
         ? (
           <div>
